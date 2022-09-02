9062712efaa3232d8a8261dab5feeb7ab2d3f3c5
toan-chu-ps <85865975+toan-chu-ps@users.noreply.github.com>
2022-09-02 17:12:47 +0700

    [Fix] Some additional things

--- Assets/scripts/gameplay/cards/card_effects/SO_CardEffect.cs
+++ Assets/scripts/gameplay/cards/card_effects/SO_CardEffect.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 
 namespace MainGame
 {
-    [CreateAssetMenu(fileName = "cardinfo", menuName = "ScriptableObjects/Card Effects/Effect", order = 1)]
-
     public abstract class SO_CardEffect : ScriptableObject
     {
         public abstract void ActivateEffect();
---
+++ Assets/scripts/gameplay/cards/card_effects/SO_HealthModifierEffect.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace MainGame
+{
+    [CreateAssetMenu(fileName = "cardeffect", menuName = "ScriptableObjects/Card Effects/Health Modifier", order = 1)]
+    public class SO_HealthModifierEffect : SO_CardEffect
+    {
+        public override void ActivateEffect()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
